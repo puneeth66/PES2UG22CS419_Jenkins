c1d97ec5431127dcc993dc03983d73da17ff2f93
Puneeth BU <150943839+puneeth66@users.noreply.github.com>
2025-03-17 15:48:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello DBOSS";
+  cout << "Hello JALGARA";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
